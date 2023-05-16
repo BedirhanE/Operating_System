de43bcad8055a116331c9376f4e0d003534553e4
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-05-16 14:06:25 +0300

    Update Project

--- deneme1.py
+++ deneme1.py
@@ -1,15 +1,20 @@
 import time
 import threading
 
+
 #using Thhreads
 class PhoneStation:
-    def __init__(self):
+    def __init__(self):#Lock nesneleri oluşturdum.
         self.lady1 = threading.Lock()
         self.lady2 = threading.Lock()
         self.line1 = threading.Lock()
         self.line2 = threading.Lock()
         self.calls_completed = 0
 
+
+
+    # Arkadaşın aramasını yapmak için bu metot kullanılır
+    # Eğer Bayan 1 ve Hat 1 kullanılabilir durumdaysa, arama gerçekleştirilir.
     def call_friend(self, friend):
         if self.lady1.acquire(blocking=False) and self.line1.acquire(blocking=False):
             self._make_call(friend, "Line 1", "Lady 1")
@@ -18,6 +23,9 @@ class PhoneStation:
         else:
             self._wait_and_call(friend)
 
+
+
+    # Arama gerçekleştirme işlemini simüle etmek için kullanılır.
     def _make_call(self, friend, line, lady):
         print(f"Friend {friend} is talking on {line} with {lady}")
         time.sleep(1)  # Simulating the duration of the phone call.
@@ -25,6 +33,9 @@ class PhoneStation:
         self.calls_completed += 1
         self._release_line(line, lady)
 
+
+
+    # Kullanılan hat ve bayan kilidi serbest bırakılır
     def _release_line(self, line, lady):
         if line == "Line 1":
             self.line1.release()
@@ -33,6 +44,9 @@ class PhoneStation:
             self.line2.release()
             self.lady2.release()
 
+
+
+    # Bekleme işlemi gerçekleştirilir
     def _wait_and_call(self, friend):#wait operation opject.
         while True:
             if self.lady1.acquire(blocking=False):
